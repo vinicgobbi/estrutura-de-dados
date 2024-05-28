8ebc52d96d6118711490cf34bc38ffe707470e06
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2024-05-27 21:07:51 -0300

    Aula do dia 27/05

--- src/arvore/Abb.java
+++ src/arvore/Abb.java
@@ -1,28 +1,45 @@
 package arvore;
 
-import entidades.*;
+import entidades.Item;
+import entidades.NoAbb;
+import lista.LCItem;
 
 public class Abb {
+
     private NoAbb raiz;
     private int quant;
 
-    public Abb(){
+    /*
+        Essa classe, além dos gets (observe que não faremos os sets)
+        os métodos: eVAzia, pesquisar, inserir, remover 
+        e outros métodos para visitar cada nó da árvore, 
+        que veremos mais à frente. 
+     */
+    public Abb() {
         this.raiz = null;
         this.quant = 0;
     }
 
-    public boolean eVazia(){
+    public NoAbb getRaiz() {
+        return raiz;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public boolean eVazia() {
         if (this.quant == 0) {
             return true;
         }
         return false;
     }
 
-    public NoAbb pesquisa(int num){
+    public NoAbb pesquisa(int num) {
         return pesquisa(num, this.raiz);
     }
 
-    private NoAbb pesquisa(int num, NoAbb no){
+    private NoAbb pesquisa(int num, NoAbb no) {
         if (no == null) {
             return null;
         }
@@ -30,9 +47,109 @@ public class Abb {
             return pesquisa(num, no.getEsq());
         } else if (num > no.getItem().getCodigo()) {
             return pesquisa(num, no.getDir());
-        }else {
+        } else {
             return no;
         }
     }
 
+    public boolean insere(Item item) {
+        NoAbb no = pesquisa(item.getCodigo());
+        if (no == null) {
+            this.raiz = insere(item, this.raiz);
+            return true;
+        }
+        return false;
+    }
+
+    private NoAbb insere(Item item, NoAbb no) {
+        if (no == null) {
+            NoAbb novoNo = new NoAbb(item);
+            quant++;
+            return novoNo;
+        }
+        if (item.getCodigo() < no.getItem().getCodigo()) {
+            no.setEsq(insere(item, no.getEsq()));
+        } else {
+            no.setDir(insere(item, no.getDir()));
+        }
+        return no;
+    }
+
+    public NoAbb remove(int codigo) {
+        return remove(codigo, this.raiz);
+    }
+
+    private NoAbb remove(int codigo, NoAbb no) {
+        if (no == null) {
+            return null;
+        } else if (codigo < no.getItem().getCodigo()) {
+            no.setEsq(remove(codigo, no.getEsq()));
+        } else if (codigo > no.getItem().getCodigo()) {
+            no.setDir(remove(codigo, no.getDir()));
+        } else if (no.getDir() == null) {
+            quant--;
+            return no.getEsq();
+        } else if (no.getEsq() == null) {
+            quant--;
+            return no.getDir();
+        } else {
+            no.setEsq(maiorEsq(no, no.getEsq()));
+            quant--;
+        }
+        return no;
+    }
+
+    private NoAbb maiorEsq(NoAbb no, NoAbb maior) {
+        // andar para a direita procurando o maior elemento
+        // da sub-arvore da esquerda
+        if (maior.getDir() != null) {
+            maior.setDir(maiorEsq(no, maior.getDir()));
+            return maior;
+        } else {
+            no.setItem(maior.getItem());
+            return maior.getEsq();
+        }
+    }
+
+    public LCItem CamInOrdem() {
+        LCItem vetor = new LCItem(this.quant);
+        fazCamInOrdem(vetor, this.raiz);
+        return (vetor);
+    }
+
+    private void fazCamInOrdem(LCItem vetor, NoAbb no) {
+        if (no != null) {
+            fazCamInOrdem(vetor, no.getEsq());
+            vetor.insereFinal(no.getItem());
+            fazCamInOrdem(vetor, no.getDir());
+        }
+    }
+
+    public LCItem CamPreOrdem() {
+        LCItem vetor = new LCItem(this.quant);
+        fazCamPreOrdem(vetor, this.raiz);
+        return (vetor);
+    }
+
+    private void fazCamPreOrdem(LCItem vetor, NoAbb no) {
+        if (no != null) {
+            vetor.insereFinal(no.getItem());
+            fazCamPreOrdem(vetor, no.getEsq());
+            fazCamPreOrdem(vetor, no.getDir());
+        }
+    }
+    
+       public LCItem CamPosOrdem() {
+        LCItem vetor = new LCItem(this.quant);
+        fazCamPosOrdem(vetor, this.raiz);
+        return (vetor);
+    }
+
+    private void fazCamPosOrdem(LCItem vetor, NoAbb no) {
+        if (no != null) {
+            fazCamPosOrdem(vetor, no.getEsq());
+            fazCamPosOrdem(vetor, no.getDir());
+            vetor.insereFinal(no.getItem());
+        }
+    }
 }
--- src/entidades/NoAbb.java
+++ src/entidades/NoAbb.java
@@ -4,34 +4,39 @@ public class NoAbb {
     private Item item;
     private NoAbb esq, dir;
 
-    public NoAbb getDir() {
-        return dir;
-    }
-
-    public NoAbb getEsq() {
-        return esq;
-    }
-
-    public void setDir(NoAbb dir) {
-        this.dir = dir;
-    }
-
-    public void setEsq(NoAbb esq) {
-        this.esq = esq;
+    public NoAbb(Item item) {
+        this.item = item;
+        this.dir = null;
+        this.esq = null;
     }
 
     public Item getItem() {
         return item;
     }
 
+    public NoAbb getEsq() {
+        return esq;
+    }
+
+    public NoAbb getDir() {
+        return dir;
+    }
+
     public void setItem(Item item) {
         this.item = item;
     }
 
+    public void setEsq(NoAbb esq) {
+        this.esq = esq;
+    }
+
+    public void setDir(NoAbb dir) {
+        this.dir = dir;
+    }
+
     @Override
     public String toString() {
-        return "NoAbb{" +
-                "item=" + item +
-                '}';
+        return "NoAbb{" + "item=" + item + '}';
     }
+
 }
